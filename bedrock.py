9c17c399bdc5d2dd01a4d229cf9df8f981cfbe25
Shipragit <shiva291291@gmail.com>
2026-05-08 15:09:04 +0530

    adding print statement in bedrock

---
+++ bedrock.py
@@ -0,0 +1,56 @@
+import sys
+import json
+import boto3
+
+client = boto3.client(
+    "bedrock-runtime",
+    region_name="us-east-1"
+)
+
+print("analyzing the report")
+model_id = "anthropic.claude-3-haiku-20240307-v1:0"
+
+file_path = sys.argv[1]
+
+with open(file_path, "r") as f:
+    report_json = json.load(f)
+
+report_data = json.dumps(report_json)
+
+prompt = f"""
+Analyze this Playwright test report.
+
+Provide:
+1. Failed test summary
+2. Root cause
+3. Suggested fix
+
+Report:
+{report_data[:12000]}
+"""
+
+native_request = {
+    "anthropic_version": "bedrock-2023-05-31",
+    "max_tokens": 512,
+    "temperature": 0.5,
+    "messages": [
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": prompt
+                }
+            ]
+        }
+    ]
+}
+
+response = client.invoke_model(
+    modelId=model_id,
+    body=json.dumps(native_request)
+)
+
+model_response = json.loads(response["body"].read())
+
+print(model_response["content"][0]["text"])
